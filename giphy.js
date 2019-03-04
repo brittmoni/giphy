94d0e3f467915253fc94ea107174aa4a66877a18
Brittany F <brittfranklin92@gmail.com>
2019-03-03 22:38:16 -0500

    JavaScript

--- giphy.js
+++ giphy.js
@@ -1,1 +1,25 @@
-var topics = [];
+var topics = ['Scarface', 'Juice'];
+
+function addMovieButton() {
+  $('#movie-buttons').empty();
+
+  for (var i = 0; i < topics.length; i++) {
+    var movieBtn = $('<button>');
+    movieBtn.addClass('btn btn-secondary new-movie')
+      .attr('data-name', topics[i])
+      .text(topics[i]);
+    
+      $('#movie-buttons').append(movieBtn);
+  }
+}
+
+$('#enter').on('click', function(event) {
+  event.preventDefault();
+
+  var movie = $('#new-movie').val().trim();
+  topics.push(movie);
+
+  addMovieButton();
+})
+
+addMovieButton();
